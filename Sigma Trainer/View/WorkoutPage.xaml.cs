e5ddc5556c2e24a53551cb295c65a4296f22be2e
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-03-30 11:20:48 +0300

    Добавил отображение упражнений с помощью CollectionView и возможность добавлять им счёт (в статистику)

--- Sigma Trainer/View/WorkoutPage.xaml.cs
+++ Sigma Trainer/View/WorkoutPage.xaml.cs
@@ -4,9 +4,16 @@ namespace Sigma_Trainer.View;
 
 public partial class WorkoutPage : ContentPage
 {
+	private WorkoutViewModel _viewModel;
 	public WorkoutPage(WorkoutViewModel vm)
 	{
 		BindingContext = vm;
+		_viewModel = vm;
 		InitializeComponent();
 	}
+    protected override async void OnAppearing()
+    {
+	 	await _viewModel.UpdateExerciseList();
+        base.OnAppearing();
+    }
 }
--- Sigma Trainer/ViewModel/WorkoutViewModel.cs
+++ Sigma Trainer/ViewModel/WorkoutViewModel.cs
@@ -1,11 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using DBLibrary.Entities;
 using Sigma_Trainer.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace Sigma_Trainer.ViewModel
 {
@@ -13,21 +10,47 @@ namespace Sigma_Trainer.ViewModel
     {
         private readonly StatisticsService _statisticsService;
         private readonly ExerciseService _exerciseService;
+        [ObservableProperty]
+        ObservableCollection<Exercises> exercises;
         public WorkoutViewModel(StatisticsService statisticsService, ExerciseService exerciseService)
         {
             _statisticsService = statisticsService;
             _exerciseService = exerciseService;
         }
         [RelayCommand]
-        public async Task AddExerciseAsync(string name, string description)
+        public async Task AddExercise()
         {
-            name = "Отжимания";
-            description = "Упражнения крутое реально!!";
+            var name = "Подтягивания";
+            var description = "Упражнения крутое реально!!";
             await _exerciseService.AddExerciseAsync(new DBLibrary.Entities.Exercises { Description = description, Name = name });
         }
+        [RelayCommand]
         public async Task GetExercise()
         {
             var exercises = await _exerciseService.GetExercises();
+        }
+        [RelayCommand]
+        public async Task AddScore(int exerciseId)
+        {
+            var result = await Application.Current.MainPage.DisplayPromptAsync("Добавить счёт",
+                "Введите количество очков:",
+                "OK",
+                "Отмена",
+                keyboard: Keyboard.Numeric);
+            // Проверяем, что пользователь ввёл значение и оно корректно преобразуется в число
+            if (result != null && int.TryParse(result, out int score))
+            {
+                if (score > 0)
+                {
+                    await _statisticsService.AddExerciseStatisticsAsync(exerciseId, score);
+
+                    var r = await _statisticsService.GetExerciseStatisticsAsync(exerciseId);
+                }
+            }
+        }
+        public async Task UpdateExerciseList()
+        {
+            Exercises = new ObservableCollection<Exercises>(await _exerciseService.GetExercises());
         }
     }
 }
